01cd4014f58184487e211a68510e95bbed3dfa95
khanmdsohel02 <khanmdsohel02.25@gmail.com>
2022-03-26 21:49:37 +0600

    korte parlam na

--- src/components/Sideber/Sideber.js
+++ src/components/Sideber/Sideber.js
@@ -10,12 +10,9 @@ const Sideber = (props) => {
     for(const gun of cart){
         Images = gun.img;
          names =gun.name;
-        console.log(Images,      
-            );
+        //  console.log(Images, names );
+       }
 
-
-
-}
     return (
         <div className='cart'>
             <h2>Selected Guns</h2>
@@ -23,7 +20,8 @@ const Sideber = (props) => {
                 <img src={Images}  alt="" />
                 <h3>{names}</h3>
             </div>
-            
+            <button>Choose One </button>
+            <button>Reset</button>
         </div>
     );
 };
